6c09f049c4a4be38676d60a2e3711bd62c7933ff
TX <108386428+TXX588356@users.noreply.github.com>
2026-08-13 11:38:32 +0800

    Update image_storage.go

--- internal/interfaces/image_storage.go
+++ internal/interfaces/image_storage.go
@@ -7,6 +7,7 @@ import (
 
 type ImageStorage interface {
 	UploadMealImage(ctx context.Context, reader io.Reader, size int64, contentType string) (objectName, publicURL string, err error)
+	PresignMealImage(ctx context.Context, imageURL string) (string, error)
 	DeleteObject(ctx context.Context, objectName string) error
 	DeleteMealImage(ctx context.Context, imageURL string) error
 }
