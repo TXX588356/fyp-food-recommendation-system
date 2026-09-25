3f9411ca73efd9938499eb9ed2887532d32a34fa
tx <leetianxin04@gmail.com>
2026-09-25 14:43:24 +0800

    chore: serve frontend assets from runtime static dir

--- static/embed.go
+++
@@ -1,8 +0,0 @@
-package static
-
-import "embed"
-
-// FS contains the compiled React frontend for serverless deployments.
-//
-//go:embed index.html assets/* dish.png favicon.svg icons.svg malaysia_cities.csv
-var FS embed.FS
